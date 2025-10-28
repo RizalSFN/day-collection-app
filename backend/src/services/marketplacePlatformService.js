c6184f60934b3d64bc26ed2f245c517523a3ce66
Rizal <rizalsofiana1976@gmail.com>
2025-10-28 20:20:48 +0700

    create marketplace platform service

---
+++ backend/src/services/marketplacePlatformService.js
@@ -0,0 +1,38 @@
+import prisma from "../config/db.js";
+
+export const createMarketplacePlatform = async (data) => {
+    return await prisma.marketplace_platform.create({
+        data: {
+            name: data.name
+        }
+    })
+}
+
+export const getAllMarketplacePlatform = async () => {
+    return await prisma.marketplace_platform.find({
+        include: { marketplace_link: true },
+        orderBy: { id: "asc" }
+    })
+}
+
+export const getByIdMarketplacePlatform = async (id) => {
+    return await prisma.marketplace_platform.findUnique({
+        where: { id: Number(id) },
+        include: { marketplace_link: true }
+    })
+}
+
+export const updateMarketplacePlatform = async (id, data) => {
+    return await prisma.marketplace_platform.update({
+        where: { id: Number(id) },
+        data: {
+            name: data.name
+        }
+    })
+}
+
+export const deleteMarketplacePlatform = async (id) => {
+    return await prisma.marketplace_platform.delete({
+        where: { id: Number(id) }
+    })
+}
