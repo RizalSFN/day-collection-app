c340cf0f8d00e3a61a809c6e2d6d05410b2c15b2
Rizal <rizalsofiana1976@gmail.com>
2025-10-28 14:32:47 +0700

    setup middleware authentikasi

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv"
 import { errorHandler } from "./middlewares/errorMiddleware.js"
 import productRoutes from "./routes/productRoutes.js"
 import authRoutes from "./routes/authRoutes.js"
+import { authenticate } from "./middlewares/authMiddleware.js"
 
 dotenv.config()
 const app = express()
@@ -13,7 +14,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use("/api/products", productRoutes)
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authenticate, authRoutes)
 
 app.get("/", (req, res) => {
     res.send("E-Commerce API is running...")
--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -2,5 +2,19 @@ import jwt from "jsonwebtoken";
 import { errorResponse } from "../utils/responseHelper.js";
 
 export const authenticate = async (req, res, next) => {
-    
+    const authHeader = req.headers.authorization
+
+    if (!authHeader || !authHeader.startWith("Bearer ")) {
+        return errorResponse(res, "Unauthorized: No token provided", 401)
+    }
+
+    const token = authHeader.split(" ")[1]
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = decoded
+        next()
+    } catch (error) {
+        return errorResponse(res, "Unauthorized: Invalid token", 401)
+    }
 }
