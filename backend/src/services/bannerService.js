6ced235a170120d9a6d7c8ddf97be08ff3eb5a72
Rizal <rizalsofiana1976@gmail.com>
2025-11-01 18:34:59 +0700

    update banner service

--- backend/src/services/bannerService.js
+++ backend/src/services/bannerService.js
@@ -1,10 +1,18 @@
+import cloudinary from "../config/cloudinary.js";
 import prisma from "../config/db.js";
+import fs, { link } from "fs"
 
-export const createBanner = async (data) => {
+export const createBanner = async (data, filePath) => {
+    const uploadResult = await cloudinary.uploader.upload(filePath, {
+        folder: "banner"
+    })
+
+    fs.unlinkSync(filePath)
+
     return await prisma.banner.create({
         data: {
             title: data.title,
-            image_url: data.image_url,
+            image_url: uploadResult.secure_url,
             link: data.link,
             is_active: data.is_active ?? true
         }
@@ -17,14 +25,24 @@ export const getAllBanner = async () => {
     })
 }
 
-export const updateBanner = async (id, data) => {
+export const updateBanner = async (id, data, filePath) => {
+    let updateData = {
+        title: data.title,
+        link: data.link,
+        is_active: data.is_active ?? true
+    }
+
+    if (filePath) {
+        const uploadResult = await cloudinary.uploader.upload(filePath, {
+            folder: "banner"
+        })
+
+        fs.unlinkSync(filePath)
+        updateData.image_url = uploadResult.secure_url
+    }
+
     return await prisma.banner.update({
         where: { id: Number(id) },
-        data: {
-            title: data.title,
-            image_url: data.image_url,
-            link: data.link,
-            is_active: data.is_active ?? true
-        }
+        data: updateData
     })
 }
