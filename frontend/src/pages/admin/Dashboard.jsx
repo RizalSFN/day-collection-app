f77cfe21c72be712be7ae751f91601b2a0a0c5cb
Rizal <rizalsofiana1976@gmail.com>
2025-11-13 21:32:54 +0700

    update dashboard page in admin page

--- frontend/src/pages/admin/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -2,18 +2,13 @@ import React from "react";
 import Sidebar from "../../components/admin/Sidebar";
 import Navbar from "../../components/admin/Navbar";
 import DashboardContent from "../../components/admin/DashboardContent";
+import DashboardLayout from "../../layouts/DashboardLayout";
 
 const Dashboard = () => {
     return (
-        <div className="flex h-screen bg-gray-50">
-            <Sidebar />
-            <div className="flex-1 flex flex-col overflow-hidden">
-                <Navbar />
-                <main className="flex-1 overflow-y-auto">
-                    <DashboardContent />
-                </main>
-            </div>
-        </div>
+        <DashboardLayout>
+            <DashboardContent />
+        </DashboardLayout>
     )
 }
 
