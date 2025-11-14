2c9e9848a7928542bb9e42f4c06d62ada0f99d7d
Rizal <rizalsofiana1976@gmail.com>
2025-11-15 00:33:28 +0700

    update marketplace platform service

--- frontend/src/services/marketplacePlatformService.js
+++ frontend/src/services/marketplacePlatformService.js
@@ -25,6 +25,19 @@ export const createMarketplacePlatform = async (payload) => {
     }
 }
 
+export const updateMarketplacePlatform = async (id, data) => {
+    try {
+        const token = getToken()
+        const response = await api.put(`/marketplace-platform/${id}`, data, {
+            headers: { Authorization: `Bearer ${token}` }
+        })
+        return response.data
+    } catch (error) {
+        console.log("Gagal mengubah data marketplace platform", error);
+        return []
+    }
+}
+
 export const deleteMarketplacePlatform = async (id) => {
     try {
         const token = getToken()
