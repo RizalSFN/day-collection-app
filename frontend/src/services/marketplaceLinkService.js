6e3e0f03e7cf8558242fd5c63f4191b5995430fd
Rizal <rizalsofiana1976@gmail.com>
2025-11-16 14:50:57 +0700

    update marketplace link service

--- frontend/src/services/marketplaceLinkService.js
+++ frontend/src/services/marketplaceLinkService.js
@@ -1,3 +1,4 @@
+import { getToken } from "../utils/storage";
 import api from "./api";
 
 export const getMarketplaceLink = async () => {
@@ -33,6 +34,55 @@ export const getMarketplaceLink = async () => {
         return Object.values(grouped);
     } catch (error) {
         console.error("Gagal memuat data banner: ", error);
+        return []
+    }
+}
+
+export const getOriginallyMarketplaceLink = async () => {
+    try {
+        const response = await api.get("/marketplace-link")
+        return response.data.data
+    } catch (error) {
+        console.log("Gagal memuat data marketplace link", error);
+        return []
+    }
+}
+
+export const createMarketplaceLink = async (payload) => {
+    try {
+        const token = getToken()
+        const response = await api.post("/marketplace-link", payload, {
+            headers: { Authorization: `Bearer ${token}` }
+        })
+        return response.data
+    } catch (error) {
+        console.log("Gagal menambah data marketplace link", error);
+        return []
+    }
+}
+
+export const updateMarketplaceLink = async (id, data) => {
+    try {
+        const token = getToken()
+        const response = await api.put(`/marketplace-link/${id}`, data, {
+            headers: { Authorization: `Bearer ${token}` }
+        })
+        return response.data
+    } catch (error) {
+        console.log("Gagal mengubah data marketplace link", error);
+        return []
+    }
+}
+
+export const deleteMarketplaceLink = async (id) => {
+    try {
+        const token = getToken()
+        await api.delete(`/marketplace-link/${id}`, {
+            headers: { Authorization: `Bearer ${token}` }
+        })
+        return true
+    } catch (error) {
+        console.log("Gagal menghapus data marketplace link", error);
         return []
     }
 }
