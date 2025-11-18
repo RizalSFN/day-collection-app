73913d145fb5cbd03bf006bb38bec5c4e4d33cc0
Rizal <rizalsofiana1976@gmail.com>
2025-11-18 23:30:20 +0700

    update app setting service

--- frontend/src/services/appSettingService.js
+++ frontend/src/services/appSettingService.js
@@ -1,3 +1,4 @@
+import { getToken } from "../utils/storage.js";
 import api from "./api.js";
 
 export const getAppSetting = async () => {
@@ -6,6 +7,32 @@ export const getAppSetting = async () => {
         return response.data.data
     } catch (error) {
         console.error("Gagal memuat data app setting: ", error);
+        return []
+    }
+}
+
+export const createAppSetting = async (payload) => {
+    try {
+        const token = getToken()
+        const response = await api.post("/app-setting", payload, {
+            headers: { Authorization: `Bearer ${token}` }
+        })
+        return response.data
+    } catch (error) {
+        console.log("Gagal menambahkan data app setting", error);
+        return []
+    }
+}
+
+export const updateAppSetting = async (id, data) => {
+    try {
+        const token = getToken()
+        const response = await api.put(`/app-setting/${id}`, data, {
+            headers: { Authorization: `Bearer ${token}` }
+        })
+        return response.data
+    } catch (error) {
+        console.log("Gagal mengubah data app setting", error);
         return []
     }
 }
