56af1cdbaac93e340ba4e2c1bd8ccd5cd7c0b675
Rizal <rizalsofiana1976@gmail.com>
2025-12-03 21:40:41 +0700

    update banner routes

--- backend/src/routes/bannerRoutes.js
+++ backend/src/routes/bannerRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import { upload } from "../middlewares/uploadMiddleware.js";
 import {
     createBanner,
+    getActiveBanner,
     getAllBanner,
     updateBanner
 } from "../controllers/bannerController.js";
@@ -10,6 +11,7 @@ import { authenticate } from "../middlewares/authMiddleware.js";
 const bannerRoutes = express.Router()
 
 bannerRoutes.get("/", getAllBanner)
+bannerRoutes.get("/active", getActiveBanner)
 
 bannerRoutes.post("/", authenticate, upload.single("image_url"), createBanner)
 bannerRoutes.put("/:id", authenticate, upload.single("image_url"), updateBanner)
