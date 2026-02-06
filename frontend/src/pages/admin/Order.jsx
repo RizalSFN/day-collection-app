3383d37ea0f0ef2c31e778ad88984c6f9e468f71
Rizal <rizalsofiana1976@gmail.com>
2026-02-06 22:19:41 +0700

    add confirmation delete on order page (admin) part 3

--- frontend/src/pages/admin/Order.jsx
+++ frontend/src/pages/admin/Order.jsx
@@ -157,7 +157,7 @@ const Order = () => {
 
             {/* Modal Detail & Verifikasi Pembayaran */}
             {isDetailModalOpen && selectedOrder && (
-                <div className="fixed inset-0 bg-black/40 backdrop-blur-sm z-40 flex items-center justify-center p-4">
+                <div className="fixed inset-0 bg-black/40 backdrop-blur-sm z-50 flex items-center justify-center p-4">
                     <div className="bg-white w-full max-w-4xl rounded-[2.5rem] shadow-2xl p-8 max-h-[90vh] overflow-y-auto scrollbar-hide">
                         <div className="flex justify-between items-start mb-8">
                             <div>
@@ -257,7 +257,7 @@ const Order = () => {
 
             {/* Modal Konfirmasi Update Status */}
             {isConfirmModalOpen && (
-                <div className="fixed inset-0 bg-black/40 backdrop-blur-sm z-50 flex items-center justify-center p-4">
+                <div className="fixed inset-0 bg-black/40 backdrop-blur-sm z-60 flex items-center justify-center p-4">
                     <div className="bg-white w-full max-w-sm rounded-[2.5rem] p-8 shadow-2xl animate-fadeIn text-center">
 
                         {/* Ikon & Warna Dinamis berdasarkan Status */}
