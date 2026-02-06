b7372efa9d9b8497582751707a2132298cec9e92
Rizal <rizalsofiana1976@gmail.com>
2026-02-06 22:26:05 +0700

    add animated loading in modal variant product page part 2

--- frontend/src/pages/admin/VariantProduct.jsx
+++ frontend/src/pages/admin/VariantProduct.jsx
@@ -243,7 +243,7 @@ const VariantProduct = () => {
                             <div className="pt-4">
                                 <button type="submit" className="w-full py-4 bg-gray-900 hover:bg-amber-500 text-white rounded-2xl font-black uppercase tracking-widest transition-all flex items-center justify-center gap-2">
                                     {/* <Save size={18} /> {editingVariant ? 'Update Variant' : 'Create Variant'} */}
-                                    <Save size={18} /> {loading ? editingVariant ? 'Update Variant' : 'Create Variant' : 'Memproses...'}
+                                    <Save size={18} /> {editingVariant ? loading ? 'Update Variant' : 'Memproses...' : 'Create Variant'}
                                 </button>
                             </div>
                         </form>
