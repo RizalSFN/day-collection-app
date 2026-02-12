7b96beee126bffa454a3ca3cbff691ffb2e9436b
Rizal <rizalsofiana1976@gmail.com>
2026-02-12 20:36:40 +0700

    update logic cek ongkir part 2

--- backend/src/services/rajaongkirService.js
+++ backend/src/services/rajaongkirService.js
@@ -40,7 +40,7 @@ export const calculateCostService = async (origin, destination, weight, courier)
         // Ini kuncinya! Jangan kirim object JSON mentah.
         const params = new URLSearchParams();
         params.append("origin", cleanOrigin);
-        params.append("originType", "subdistrict")
+        params.append("originType", "city")
         params.append("destination", cleanDest);
         params.append("destinationType", "subdistrict")
         params.append("weight", cleanWeight);
