9459f242b190530fdfd90190fc21fef25c532e8a
Rizal <rizalsofiana1976@gmail.com>
2026-02-16 14:20:43 +0700

    update color basic

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -3,7 +3,7 @@ import Footer from "../components/common/Footer";
 
 export default function MainLayout({ children }) {
     return (
-        <div className="min-h-screen bg-yellow-50 flex flex-col">
+        <div className="min-h-screen flex flex-col">
             <Navbar />
             <main className="flex-1 container mx-auto px-4 py-6">{children}</main>
             <Footer />
